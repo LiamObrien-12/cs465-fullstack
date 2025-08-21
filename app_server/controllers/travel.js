8e177ff1a92a69bb9bef84768ca1785b394cbfc1
Your Name <137745275+LiamObrien-12@users.noreply.github.com>
2025-08-21 01:57:51 -0400

    next

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,24 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+// API endpoint configuration
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+};
+
+// Comment out the old file reading code
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', trips });
+const travel = async function(req, res, next) {
+    await fetch(tripsEndpoint, options)
+        .then((res) => res.json())
+        .then((json) => {
+            res.render('travel', { title: 'Travlr Getaways', trips: json });
+        })
+        .catch((err) => res.status(500).send(err.message));
 };
 
 module.exports = {
